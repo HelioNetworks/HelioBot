364f1965ff3db04f383a68d36cf0a888868db317
JJE Admin <jje@area51.heliohost.org>
2011-10-26 11:20:18 +0100

    Debug 2

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+screen php -q /home1/jje/public_html/jjeadmin.co.cc/irc/index.php
 cd /home1/jje/public_html/jjeadmin.co.cc/irc/
 rm cron.sh
 git pull
 chmod 0755 cron.sh
-screen php -q /home1/jje/public_html/jjeadmin.co.cc/irc/index.php
